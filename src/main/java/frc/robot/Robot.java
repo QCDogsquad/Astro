24b52437ea008b6dbc93e96147a953656a3e230d
QC DogSquad <qcdogsquad@gmail.com>
2019-10-15 14:36:53 -0700

    Comments

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -13,30 +13,35 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Robot extends TimedRobot {
+    // Constants for the autonomous period
     private final String c_autoDefault = "Default";
-    private final String c_autoPaths   = "Paths";
+    // The different autonomous modes are handled in a switch statement
     private String m_autoSelected;
     private final SendableChooser<String> m_chooser = new SendableChooser<>();
 
     // This is the main joystick
     Joystick m_flightStick = new Joystick(0);
+    // Axis(0): X axis (left and right)
+    // Axis(1): Y axis (forward and backwards)
+    // Axis(2): Z axis (rotation)
+    // Axis(3): Slider (rotating flap thing at the back of controller)
 
-    // Dogtail controllers
+    // Dogtail motor/speed controllers
     Victor m_dogtailLeft  = new Victor(4);
     Victor m_dogtailRight = new Victor(5);
-    Spark  m_dogtailPos   = new Spark(7); // Motor that controls the dogtail's position
+    // Motor that controls if the dogtail if raised or lowered
+    Spark  m_dogtailPos   = new Spark(7);
 
-    // Drivetrain speed controllers
+    // Drivetrain motor/speed controllers
     Spark m_leftFront  = new Spark(0); // Green motor
     Spark m_leftBack   = new Spark(1); // Yellow motor
     Spark m_rightFront = new Spark(2); // Blue motor
     Spark m_rightBack  = new Spark(3); // White motor
 
-    // Group motors into sides
-    SpeedControllerGroup m_motorsLeft = new SpeedControllerGroup(m_leftFront, m_leftBack);
+    // Group drivetrain motors into sides
+    SpeedControllerGroup m_motorsLeft  = new SpeedControllerGroup(m_leftFront, m_leftBack);
     SpeedControllerGroup m_motorsRight = new SpeedControllerGroup(m_rightFront, m_rightBack);
-
-    // Group sides together together
+    // Group motor sides together together
     DifferentialDrive m_drive = new DifferentialDrive(m_motorsLeft, m_motorsRight);
 
     // Compressor
@@ -57,7 +62,6 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
         CameraServer.getInstance().startAutomaticCapture();
-        m_chooser.addOption("Path Auto", c_autoPaths);
         m_chooser.setDefaultOption("Default Auto", c_autoDefault);
         SmartDashboard.putData("Auto choices", m_chooser);
         System.out.println("** // Robot Ready // **");
@@ -83,43 +87,50 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * CUSTOM This function is a custom one that is used for raising or lowering a
+     * CUSTOM 
+     * This function is a custom one that is used for raising or lowering a
      * certain solenoid based up a pair of buttons This function abstracts this away
      * so that the same code is not written 4 times for each solenoid
      */
     private void activateSolenoid(boolean extend, boolean retract, DoubleSolenoid solenoid) {
-        if (extend == retract) {
+        if (extend == retract) // We don't want to make a decision if both triggers are equal
             solenoid.set(DoubleSolenoid.Value.kOff);
-        } else if (extend) {
+        else if (extend)
             solenoid.set(DoubleSolenoid.Value.kForward);
-        } else if (retract) {
+        else if (retract)
             m_solenoidClawPos.set(DoubleSolenoid.Value.kReverse);
-        }
     }
 
     /**
-     * CUSTOM This function is a custom one that contain all the code for
+     * CUSTOM 
+     * This function is a custom one that contain all the code for
      * controlling the robot This is to prevent rewriting the exact same code within
-     * "autonomousPeriodic()" and "teleopPeriodic()"
+     * "autonomousPeriodic()" and "teleopPeriodic()" because of the ability for the driver
+     * to control the robot (able to see only with the camera) during the autonomous period
      */
     private void defaultControl() { 
         // Main robot movement controls
-        double robotForward = m_flightStick.getRawAxis(0); // X axis is rotation
-        double robotRotate  = m_flightStick.getRawAxis(1); // Y axis is throttle
-        m_drive.arcadeDrive(robotForward, robotRotate);
+        double robotRotate  = m_flightStick.getRawAxis(0); // X axis is throttle
+        double robotForward = m_flightStick.getRawAxis(1); // Y axis is rotation
+        m_drive.arcadeDrive(-robotForward, robotRotate); 
 
         // Dogtail movement controls
-        boolean dogtailForward = m_flightStick.getRawButton(5);
+        boolean dogtailForward  = m_flightStick.getRawButton(5);
         boolean dogtailBackward = m_flightStick.getRawButton(3);
 
         final double dogtailMoveSpeed = 1.0;
-        if (dogtailForward == dogtailBackward) {
+        if (dogtailForward == dogtailBackward) // Don't want to make a decision if the buttons in the same state
+        {
             m_dogtailLeft.set(0);
             m_dogtailRight.set(0);
-        } else if (dogtailForward) {
+        }
+        else if (dogtailForward) 
+        {
             m_dogtailLeft.set(-dogtailMoveSpeed);
             m_dogtailRight.set(dogtailMoveSpeed);
-        } else if (dogtailBackward) {
+        }
+        else if (dogtailBackward) 
+        {
             m_dogtailLeft.set(dogtailMoveSpeed);
             m_dogtailRight.set(-dogtailMoveSpeed);
         }
@@ -129,28 +140,28 @@ public class Robot extends TimedRobot {
         boolean dogtailRaise = m_flightStick.getRawButton(9);
 
         final double dogtailPosSpeed = .60;
-        if (dogtailLower == dogtailRaise) {
+        if (dogtailLower == dogtailRaise) // Don't make a decision if the buttons are in the same state
             m_dogtailPos.set(0);
-        } else if (dogtailLower) {
+        else if (dogtailLower)
             m_dogtailPos.set(-dogtailPosSpeed);
-        } else if (dogtailRaise) {
+        else if (dogtailRaise)
             m_dogtailPos.set(dogtailPosSpeed);
-        }
 
-        // Claw ejector - This pops out the claw so it is now able to be used
-        boolean clawUp = m_flightStick.getRawButton(2);
+        // This controls the position of the claw (if it is up and low)
+        boolean clawUp   = m_flightStick.getRawButton(2);
         boolean clawDown = m_flightStick.getRawButton(1);
         activateSolenoid(clawUp, clawDown, m_solenoidClawPos);
 
-        // Claw extension
-        boolean clawEject = m_flightStick.getRawButton(4);
+        // This controls whether the claw is ejected or not
+        // The claw is folded in order fit within the required space
+        boolean clawEject   = m_flightStick.getRawButton(4);
         boolean clawRetract = m_flightStick.getRawButton(6);
         activateSolenoid(clawEject, clawRetract, m_solenoidClawEject);
 
         // Robot lifting controls
-        // boolean robotUp = m_flightStick.getRawButton(12);
+        // boolean robotUp   = m_flightStick.getRawButton(12);
         // boolean frontDown = m_flightStick.getRawButton(8);
-        // boolean backDown = m_flightStick.getRawButton(10);
+        // boolean backDown  = m_flightStick.getRawButton(10);
         // activateSolenoid(robotUp, frontDown, m_solenoidFrontRaise);
         // activateSolenoid(robotUp, backDown, m_solenoidBackRaise);
 
@@ -164,7 +175,7 @@ public class Robot extends TimedRobot {
         switch (m_autoSelected)
         {
             case c_autoDefault:
-            default:
+            default: // The case should never really be default
                 defaultControl();
                 break;
         }
